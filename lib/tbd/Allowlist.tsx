2d266df4f2bc492b742e15ca86297d68b1e5bf14
uidude <22799484+uidude@users.noreply.github.com>
2023-06-21 13:16:47 -0700

    Add optional User edge to AllowlistEntry

--- lib/tbd/Allowlist.tsx
+++ lib/tbd/Allowlist.tsx
@@ -1,4 +1,4 @@
-import {Role} from '@toolkit/core/api/User';
+import {Role, User} from '@toolkit/core/api/User';
 import {
   BaseModel,
   Field,
@@ -8,16 +8,21 @@ import {
 } from '@toolkit/data/DataStore';
 
 /**
- * Allowlist is a data stucture to give permissions and access to the app for
- * users before they have signed up for an account.
+ * Allowlist is a data stucture to give permissions and access to the app.
  *
- * It uses an email or phone # key to identify the user, and provides
- * a list of roles for the user.
+ * It supports assigning roles to users by email, phone, or 3P account ID
+ * before they have signed up, or by user ID after they have signed up.
+ *
+ * TODO: Rename this to a more generic term.
  */
 @Model({name: 'allowlist'})
 export class AllowlistEntry extends BaseModel {
   /** User Key can be email, phone, or user ID depending on login type */
   @Field() userKey: string;
+
   /** Roles is array of roles for that user */
   @Field(TArray(TString)) roles: Role[];
+
+  /** Signed in user ths entry applies to. */
+  @Field() user?: User;
 }
